8f4fc40c5e22c44e80f0f90d18cd722ebd69c1d2
mario <mario.mascorda@gmail.com>
2021-06-14 22:07:34 +0200

    testfinales

--- test/operaciones/OperacionesTest.java
+++ test/operaciones/OperacionesTest.java
@@ -57,6 +57,42 @@ public class OperacionesTest {
     }
 
     /**
+     * Test of sumarPar method, of class Operaciones.
+     */
+    @Test
+    @Ignore
+    public void testSumarPar2() {
+        System.out.println("sumarPar");
+        Operaciones instance = new Operaciones();
+        
+        int a = 2;
+        int b = 1;
+        
+        int expResult = 2;
+        int result = instance.sumarPar(a, b);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of sumarPar method, of class Operaciones.
+     */
+    @Test
+    @Ignore
+    public void testSumarPar3() {
+        System.out.println("sumarPar");
+        Operaciones instance = new Operaciones();
+        
+        int a = 1;
+        int b = 2;
+        
+        int expResult = 2;
+        int result = instance.sumarPar(a, b);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
      * Test of mayor method, of class Operaciones.
      */
     @Test
@@ -68,6 +104,38 @@ public class OperacionesTest {
         int b = 3;
         
         int expResult = 3;
+        int result = instance.mayor(a, b);
+        assertEquals(expResult, result);
+        
+    }
+    /**
+     * Test of mayor method, of class Operaciones.
+     */
+    @Test
+    @Ignore
+    public void testMayor2() {
+        System.out.println("mayor");
+        Operaciones instance = new Operaciones();
+        int a = 4;
+        int b = 3;
+        
+        int expResult = 4;
+        int result = instance.mayor(a, b);
+        assertEquals(expResult, result);
+        
+    }
+    /**
+     * Test of mayor method, of class Operaciones.
+     */
+    @Test
+    
+    public void testMayor3() {
+        System.out.println("mayor");
+        Operaciones instance = new Operaciones();
+        int a = 3;
+        int b = 4;
+        
+        int expResult = 4;
         int result = instance.mayor(a, b);
         assertEquals(expResult, result);
         
@@ -84,6 +152,35 @@ public class OperacionesTest {
         int[] numeros = {3,2,4,6};
         
         int expResult = 15;
+        int result = instance.sumarVector(numeros);
+        assertEquals(expResult, result);
+        
+    }/**
+     * Test of sumarVector method, of class Operaciones.
+     */
+    @Test
+    
+    public void testSumarVector2() {
+        System.out.println("sumarVector");
+        Operaciones instance = new Operaciones();
+        int[] numeros = {3,2,4,5};
+        
+        int expResult = 14;
+        int result = instance.sumarVector(numeros);
+        assertEquals(expResult, result);
+        
+    }
+    /**
+     * Test of sumarVector method, of class Operaciones.
+     */
+    @Test
+    
+    public void testSumarVector3() {
+        System.out.println("sumarVector");
+        Operaciones instance = new Operaciones();
+        int[] numeros = {1,2,4,6};
+        
+        int expResult = 13;
         int result = instance.sumarVector(numeros);
         assertEquals(expResult, result);
         
